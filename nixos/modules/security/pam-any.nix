ee58b8a04bbdcfbad3a7d08d2cf2a116e1f02c30
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-08-12 21:07:27 -0700

    Autoformatter

--- nixos/modules/security/pam-any.nix
+++ nixos/modules/security/pam-any.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 
@@ -6,12 +11,16 @@ let
   cfg = config.security.pam-any;
 
   # Helper function to generate pam-any JSON config
-  mkPamAnyConfig = name: serviceCfg: pkgs.writeText "pam-any-${name}.json" (builtins.toJSON {
-    mode = serviceCfg.mode;
-    modules = serviceCfg.modules;
-  });
+  mkPamAnyConfig =
+    name: serviceCfg:
+    pkgs.writeText "pam-any-${name}.json" (
+      builtins.toJSON {
+        mode = serviceCfg.mode;
+        modules = serviceCfg.modules;
+      }
+    );
 in
-  {
+{
   options = {
     security.pam-any = {
       enable = mkEnableOption "pam-any module for parallel PAM authentication";
@@ -24,58 +33,70 @@ in
       };
 
       services = mkOption {
-        type = types.attrsOf (types.submodule {
-          options = {
-            enable = mkEnableOption "pam-any for this service";
+        type = types.attrsOf (
+          types.submodule {
+            options = {
+              enable = mkEnableOption "pam-any for this service";
 
-            mode = mkOption {
-              type = types.enum [ "One" "All" ];
-              default = "One";
-              description = ''
-                Authentication mode:
-                - "One": Succeeds if ANY module succeeds (OR logic)
-                - "All": Succeeds if ALL modules succeed (AND logic)
-              '';
-            };
+              mode = mkOption {
+                type = types.enum [
+                  "One"
+                  "All"
+                ];
+                default = "One";
+                description = ''
+                  Authentication mode:
+                  - "One": Succeeds if ANY module succeeds (OR logic)
+                  - "All": Succeeds if ALL modules succeed (AND logic)
+                '';
+              };
 
-            modules = mkOption {
-              type = types.listOf (types.submodule {
-                options = {
-                  path = mkOption {
-                    type = types.str;
-                    description = "Path to the PAM module .so file";
-                  };
-                  args = mkOption {
-                    type = types.listOf types.str;
-                    default = [];
-                    description = "Arguments to pass to the PAM module";
-                  };
-                };
-              });
-              default = [];
-              description = "List of PAM modules to run in parallel";
-              example = literalExpression ''
-                [
-                  {
-                    path = "''${pkgs.fprintd}/lib/security/pam_fprintd.so";
-                    args = [];
+              modules = mkOption {
+                type = types.listOf (
+                  types.submodule {
+                    options = {
+                      path = mkOption {
+                        type = types.str;
+                        description = "Path to the PAM module .so file";
+                      };
+                      args = mkOption {
+                        type = types.listOf types.str;
+                        default = [ ];
+                        description = "Arguments to pass to the PAM module";
+                      };
+                    };
                   }
-                  {
-                    path = "pam_unix.so";
-                    args = ["nullok" "try_first_pass"];
-                  }
-                ]
-              '';
-            };
+                );
+                default = [ ];
+                description = "List of PAM modules to run in parallel";
+                example = literalExpression ''
+                  [
+                    {
+                      path = "''${pkgs.fprintd}/lib/security/pam_fprintd.so";
+                      args = [];
+                    }
+                    {
+                      path = "pam_unix.so";
+                      args = ["nullok" "try_first_pass"];
+                    }
+                  ]
+                '';
+              };
 
-            control = mkOption {
-              type = types.enum [ "required" "requisite" "sufficient" "optional" ];
-              default = "sufficient";
-              description = "PAM control flag for the pam-any module";
+              control = mkOption {
+                type = types.enum [
+                  "required"
+                  "requisite"
+                  "sufficient"
+                  "optional"
+                ];
+                default = "sufficient";
+                description = "PAM control flag for the pam-any module";
+              };
             };
-          };
-        });
-        default = {};
+          }
+        );
+        default = { };
         description = "PAM services to configure with pam-any";
       };
     };
@@ -85,7 +106,8 @@ in
     environment.systemPackages = [ cfg.package ];
 
     environment.etc = mkMerge (
-      mapAttrsToList (name: serviceCfg:
+      mapAttrsToList (
+        name: serviceCfg:
         mkIf serviceCfg.enable {
           "pam-any-${name}.json" = {
             source = mkPamAnyConfig name serviceCfg;
@@ -96,7 +118,8 @@ in
     );
 
     security.pam.services = mkMerge (
-      mapAttrsToList (name: serviceCfg:
+      mapAttrsToList (
+        name: serviceCfg:
         mkIf serviceCfg.enable {
           ${name} = {
             text = mkDefault (mkBefore ''
